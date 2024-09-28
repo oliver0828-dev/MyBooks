86fca8ab55ae956884abca46c1c710bdaf715a3e
Oliver Park <oliverco0828@gmail.com>
2024-09-28 23:34:33 +0900

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Oliver Park on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBooksTests/MyBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksTests.swift
+//  MyBooksTests
+//
+//  Created by Oliver Park on 9/28/24.
+//
+
+import Testing
+@testable import MyBooks
+
+struct MyBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
